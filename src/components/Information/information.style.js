98fa2867aa5eda682b124ac5f30697fb90977d82
马蕾 <malei@maleideMacBook-Pro.local>
2024-06-10 14:37:07 +0800

    feat: The director's message module has been developed

--- src/components/Information/information.style.js
+++ src/components/Information/information.style.js
@@ -1,23 +1,100 @@
 import styled from "@emotion/styled";
-import lace1 from "../../assets/image/lace1.png";
+import letterBg1 from "../../assets/image/letter-bg1.png";
+import letterBg2 from "../../assets/image/letter-bg2.png";
 
 const Container = styled.div`
-	padding: 0 1.7rem;
+	margin-top: 60px;
 
-	h4 {
-		width: 75%;
-		height: 108px;
-		line-height: 108px;
-		position: relative;
-		left: 0;
-		right: 0;
-		margin: auto;
+	.img-box {
+		margin: 0 1.7rem;
 		text-align: center;
-		background: url(${lace1}) no-repeat center;
-		background-size: contain;
-		font-size: 1.8rem;
-		box-sizing: border-box;
-		padding-top: 10px;
+		position: relative;
+		height: 185px;
+		overflow-y: clip;
+
+		.lace-img {
+			width: 75%;
+		}
+
+		.men-img {
+			width: 48%;
+			position: absolute;
+			left: -0.6rem;
+			top: 4rem;
+		}
+	}
+
+	.letter {
+		margin-top: -47px;
+		position: relative;
+
+		h4 {
+			margin: 0 1.7rem;
+			font-size: 1.8rem;
+			text-align: center;
+			padding: 7px 0;
+			background: rgba(255, 255, 255, 0.7);
+		}
+
+		.content {
+			position: relative;
+			height: 566px;
+			background: url(${letterBg1}) no-repeat;
+			background-size: contain;
+			padding: 2.3rem;
+			margin: 0 0.6rem;
+			box-sizing: border-box;
+
+			p {
+				margin: 0;
+				font-size: 1.38rem;
+				line-height: 2rem;
+				text-indent: 1rem;
+			}
+
+			.show-more {
+				color: #497cff;
+				font-size: 1.53rem;
+				display: inline-block;
+				text-decoration: underline;
+				position: absolute;
+				right: 3rem;
+				margin-top: 1.5rem;
+			}
+		}
+
+		.long-content {
+			height: 1130px;
+			background-image: url(${letterBg2}), url(${letterBg1});
+			background-position: top left, bottom right;
+			background-repeat: no-repeat;
+
+			.bottom {
+				position: absolute;
+				right: 3rem;
+				margin-top: 3.5rem;
+				text-align: center;
+
+				p {
+					line-height: 2.5rem;
+				}
+			}
+		}
+	}
+
+	@media screen and (max-width: 375px) {
+		.letter {
+			.content {
+				p {
+					font-size: 1.2rem;
+					line-height: 1.8rem;
+				}
+			}
+
+			.long-content {
+				height: 1072px;
+			}
+		}
 	}
 `;
 export { Container };
